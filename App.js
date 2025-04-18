c14a85936806acf6fbfd43cf6d639dbd13c3e216
VitalikNeznayko <vtk241_nvk@student.ztu.edu.ua>
2025-04-18 16:37:28 +0300

    push notification is configured

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { View, Text } from "react-native";
+// Використовуємо правильний імпорт для Expo
+import { OneSignal, LogLevel } from "react-native-onesignal";
 
 export default function App() {
+   useEffect(() => {
+     // Enable verbose logging for debugging (remove in production)
+     OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+     // Initialize with your OneSignal App ID
+     OneSignal.initialize("13396ec7-9133-43d0-b3cd-67d36813054c");
+     // Use this method to prompt for push notifications.
+     // We recommend removing this method after testing and instead use In-App Messages to prompt for notification permission.
+     OneSignal.Notifications.requestPermission(false);
+   }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Welcome to Your App</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
